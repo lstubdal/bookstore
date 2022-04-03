762906482dcc77c9514360bc66eea938cbe5b688
lstubdal <lotte.stubdal@gmail.com>
2022-04-03 09:54:00 +0200

    Add header comp.

--- app/src/components/Frontpage.vue
+++ app/src/components/Frontpage.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>vue boilerplate </div>
+    <div class="frontpage">
+       <Header />
+       
+    </div>
 </template>
 
 <script>
+    import Header from '../components/Header.vue';
 
     export default {
+        data() {
+            return {
+            
+            }
+        },
+
+        components: {
+            Header
+        }
    
     }
 </script>
 
 <style>
+    .frontpage {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: var(--normal) solid var(--dark);
+        margin: var(--small);
+        font-family: var(--main-font);
+    }
 </style>
---
+++ app/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+        <header class="header">
+            <h1 class="header__title">{{ title }}</h1>
+
+            <!-- make own component? -->
+            <div class="cart">
+                <img src="/icons/cart.svg" alt="cart icon">
+                <p>(items)</p>
+                <p>shoppingcart</p>
+            </div>
+        </header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'THE NORWEGIAN BOOKSTORE'
+            }
+        }
+    }
+</script>
+
+<style>
+    .header {
+        display: flex;
+    }
+
+    .header__title {
+        color: var(--highlight);
+        font-size: 2.5em;
+    }
+
+    .cart {
+        display: grid;
+        grid-template-columns: repeat(2, 33%);
+        align-items: center;
+    }
+</style>
