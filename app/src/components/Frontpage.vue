08921450f60325abdab67acd2e16bce263ea04c0
lstubdal <lotte.stubdal@gmail.com>
2022-04-03 10:11:04 +0200

    Import new sanityClient

--- app/src/components/Frontpage.vue
+++ app/src/components/Frontpage.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class="frontpage">
+    <div v-if="loading">Loading books...</div> 
+
+    <div v-else class="frontpage">
        <Header />
-       
+       <section class="frontpage__bookgenres">
+           <!-- for each genre... -->
+       </section>
     </div>
 </template>
 
 <script>
     import Header from '../components/Header.vue';
 
+    import sanityClient from '@sanity/client';
+    const sanity = sanityClient({ // create new sanityClient
+        projectId: 'cuc1osaz', // unique project id
+        dataset: 'production',
+        apiVersion: '2022-04.02', //today's date
+        useCdn: 'false' // false local / true netlify
+    })
+
     export default {
         data() {
             return {
-            
+                loading: true,
+                result: null
+
             }
         },
 
