81d84c67c5cb6ccb1116ad12777f94b23fec81d1
lstubdal <lotte.stubdal@gmail.com>
2022-04-04 19:07:28 +0200

    Create bookpreview comp

---
+++ app/src/components/BookPreview.vue
@@ -0,0 +1,98 @@
+<template>
+        <RouterLink :to="{ name: 'book', params: { book_slug: book.slug.current }}" class="preview"  @mouseover="this.hover = true">
+            <h3 class="preview__title">{{ book.title }}</h3>
+            <p class="preview__author">{{ book.author.name }}</p>
+            <img :src="book.bookCover.asset.url" alt="book cover" class="preview__image">
+            <p class="preview__price">{{ book.price }},-</p>
+        </RouterLink>
+
+        <button v-show="hover" class="preview__hover" @click="addToCart" @mouseleave="this.hover = false" >
+            <p>add to cart</p>
+            <img src="/icons/cart-small.svg" alt="mini cart icon">
+        </button>
+    
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                hover: false
+            }
+        },
+
+        props: {
+            book: {
+                type: String
+            }
+        },
+
+        methods: {
+            addToCart() {
+                window.alert('added')
+            }
+        }
+    }
+</script>
+
+<style>
+    .preview {
+        height: 300px;
+        width: 210px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: var(--margin-large);
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    .preview__title {
+        font-size: 1.3em;
+        color: var(--dark);
+        padding-bottom: var(--padding-small);
+    }
+
+    .preview__author {
+        font-size: 1.2em;
+        color: var(--main);
+        padding-bottom: var(--padding-medium);
+    }
+
+    .preview__image {
+        width: 75%;
+        height: 75%;
+        padding: var(--padding-small);
+    }
+
+    .preview__price {
+        font-size: 1.3em;
+        color: var(--main);
+        text-align: center;
+        padding: var(--padding-small);
+    }
+
+    .preview__hover {
+        height: 8%;
+        width: 12%;
+        position: absolute;
+        bottom: 20;
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        margin-left: 3.8%;
+        background: transparent;
+        color: var(--highlight);
+        border-left: none;
+        border-right: none;
+        border-top: var(--default);
+        border-bottom: var(--default);
+        border-color: var(--main);
+        cursor: pointer;
+    }
+
+    .preview__hover p {
+        font-size: 1.5em;
+    }
+</style>
--- app/src/components/Frontpage.vue
+++ app/src/components/Frontpage.vue
@@ -2,43 +2,23 @@
     <div v-if="loading" class="loadingPage">Loading books...</div> 
     <div v-else class="frontpage">
        <Header />
-       <Genres :genreList="genres"/>
+       <GenresNavigation :genreList="genres"/>
 
         <main class="frontpage__main">
             <section class="frontpage__news">
                 <h2>LATEST NEWS</h2>
 
-                <div v-for="(book,index) in latestNews" @mouseover="this.hover = true" @mouseleave="this.hover = false">
+                <div v-for="(book,index) in latestNews">
                     <div v-if="index < maxPreviewBooks">
-                        <RouterLink :to="{ name: 'book', params: { book_slug: book.slug.current }}" class="preview">
-                            <h3 class="preview__title">{{ book.title }}</h3>
-                            <p class="preview__author">{{ book.author.name }}</p>
-                            <img :src="book.bookCover.asset.url" alt="book cover" class="preview__image">
-                            <p class="preview__price">{{ book.price }},-</p>
-                        </RouterLink>
-
-                        <button v-show="hover" class="preview__hover" @click="addToCart">
-                            <p>add to cart</p>
-                            <img src="/icons/cart-small.svg" alt="mini cart icon">
-                        </button>
+                        <BookPreview :book="book" />
                     </div>
                 </div>
             </section>
 
             <section class="frontpage__popular" >
-                <div v-for="(book, index) in mostPopular" @mouseover="this.hover = true" @mouseleave="this.hover = false">
+                <div v-for="(book, index) in mostPopular">
                     <div v-if="index < maxPreviewBooks" >
-                        <RouterLink :to="{ name: 'book', params: { book_slug: book.slug.current }}" class="preview"> 
-                            <h3 class="preview__title">{{ book.title }}</h3>
-                            <p class="preview__author">{{ book.author.name }}</p>
-                            <img :src="book.bookCover.asset.url" alt="book cover" class="preview__image">
-                            <p class="preview__price">{{ book.price }},-</p>
-                        </RouterLink>
-
-                        <button v-show="hover" class="preview__hover" @click="addToCart">
-                            <p>add to cart</p>
-                            <img src="/icons/cart-small.svg" alt="mini cart icon">
-                        </button>
+                        <BookPreview :book="book" />
                     </div>
                 </div>
 
@@ -52,7 +32,8 @@
 
 <script>
     import Header from '../components/Header.vue';
-    import Genres from './GenresNavigation.vue';
+    import GenresNavigation from '../components/GenresNavigation.vue';
+    import BookPreview from '../components/BookPreview.vue';
     import Footer from '../components/Footer.vue';
 
     import sanityClient from '@sanity/client';
@@ -67,18 +48,14 @@
         data() {
             return {
                 loading: true,
-                books: null,
-                genres: null,
-                latestNews: [],
-                mostPopular: [],
                 maxPreviewBooks: 4, // limit to 4 books on frontpage
-                hover: false
             }
         },
 
         components: {
             Header,
-            Genres,
+            GenresNavigation,
+            BookPreview,
             Footer
         },
 
@@ -148,13 +125,10 @@
         },
 
         computed: {
+           
         },
 
-        methods: {
-            addToCart() {
-                window.alert('added')
-            }
-        }
+        
     }
 </script>
 
@@ -208,64 +182,5 @@
         justify-content: flex-end;
     }
 
-    .preview {
-        height: 300px;
-        width: 210px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        margin: var(--margin-large);
-        text-decoration: none;
-        cursor: pointer;
-    }
-
-    .preview__title {
-        font-size: 1.3em;
-        color: var(--dark);
-        padding-bottom: var(--padding-small);
-    }
-
-    .preview__author {
-        font-size: 1.2em;
-        color: var(--main);
-        padding-bottom: var(--padding-medium);
-    }
-
-    .preview__image {
-        width: 75%;
-        height: 75%;
-        padding: var(--padding-small);
-    }
-
-    .preview__price {
-        font-size: 1.3em;
-        color: var(--main);
-        text-align: center;
-        padding: var(--padding-small);
-    }
-
-    .preview__hover {
-        height: 8%;
-        width: 12%;
-        position: absolute;
-        bottom: 20;
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-        margin-left: 3.8%;
-        background: transparent;
-        color: var(--highlight);
-        border-left: none;
-        border-right: none;
-        border-top: var(--default);
-        border-bottom: var(--default);
-        border-color: var(--main);
-        cursor: pointer;
-    }
-
-    .preview__hover p {
-        font-size: 1.5em;
-    }
 
 </style>
