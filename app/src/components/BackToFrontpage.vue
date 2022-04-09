a29e49c293bb34da459cb435dd7fbb296711cf19
lstubdal <lotte.stubdal@gmail.com>
2022-04-09 18:06:41 +0200

    Create back to frontpage comp.

---
+++ app/src/components/BackToFrontpage.vue
@@ -0,0 +1,25 @@
+<template>
+  <RouterLink :to="{name: 'frontpage'}" class="backToFrontpage"> &#x2190; Go back to frontpage</RouterLink>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .backToFrontpage {
+        padding: var(--padding-small);
+        font-size: 1.8em;
+        color: var(--highlight);
+        margin-top: var(--margin-large);
+        text-decoration: none;
+    }
+
+    .backToFrontpage:hover {
+        color: var(--dark);
+        text-decoration: underline;
+    }
+    
+</style>
