1b05ca6ef35814b2faead8b372b0c005d0faf070
lstubdal <lotte.stubdal@gmail.com>
2022-04-30 13:44:14 +0200

    add with credentials

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -4,5 +4,11 @@ export default sanityClient({ // create new sanityClient
     projectId: 'cuc1osaz', // unique project id
     dataset: 'production',
     apiVersion: '2022-04-02', // date of sClient created
-    useCdn: 'false' // false local / true netlify
+    useCdn: 'false', // false local / true netlify
+    withCredentials: true // for sanity muations
 })
+
+
+// patch
+/* sanity.patch('id')
+        .set.commit etc */
