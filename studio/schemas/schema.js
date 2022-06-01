226081a868f8d7e60caf109ed9ded8af92ba79e6
lstubdal <lotte.stubdal@gmail.com>
2022-06-01 10:51:24 +0200

    Remove category document

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,7 +3,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import book from './documents/book'
 import genre from './documents/genre'
-import category from './documents/category'
 import author from './documents/author'
 import publisher from './documents/publisher'
 
@@ -14,6 +13,5 @@ export default createSchema({
     genre,
     author,
     publisher
-    /* category */
   ]),
 })
