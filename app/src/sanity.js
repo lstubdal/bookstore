122c346ad21f44c3cad013698a08260fbaf36cfe
lstubdal <lotte.stubdal@gmail.com>
2022-06-02 03:34:58 +0200

    Add token

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -4,6 +4,6 @@ export default sanityClient({ // create new sanityClient
     projectId: 'cuc1osaz', // unique project id
     dataset: 'production',
     apiVersion: '2022-04-02', // date of sClient created
-    useCdn: 'true', // false local / true netlify
-    withCredentials: true // for sanity muations
+    token: import.meta.env.VITE_SANITY,
+    useCdn: 'false'
 })
